39bf4d3d377db9775d46175aad3ee35a9730d02b
Efe Yanar <efe1yanar@gmail.com>
2024-04-07 00:24:03 +0300

    Add files via upload

---
+++ CarpimTablosu/CarpimTablosu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpimTablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çarpım Tablosu");
+            Console.WriteLine("");
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    Console.WriteLine(i + "x" + j + ":" + (i * j));
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
